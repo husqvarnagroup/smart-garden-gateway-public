22e15fd557ad11a2473119746f0d3ff070881dd3
Reto Schneider <code@reto-schneider.ch>
2019-08-28 00:49:21 +0200

    Allow pushing to alternative GitHub accounts

--- publicify.sh
+++ publicify.sh
@@ -2,15 +2,16 @@
 
 # This script pushes a specific version (tag) to GitHub.
 #
-# Example: ./publicify.sh release/linux-system-3.3.0.2
+# Usage:  ./publicify.sh <tag> [GitHub username]
+
+# Example: ./publicify.sh release/linux-system-3.3.0.2 husqvarnagroup
 
 set -eu -o pipefail
 
-readonly base="git@github.com:rettichschnidi"
+readonly release_tag=$1
+readonly base="git@github.com:${2-husqvarnagroup}"
 
-release_tag=$1
-
-declare -A UPSTREAM_REPOS=(
+declare -A PUBLIC_REPOS=(
     [yocto/meta-openembedded]="smart-garden-gateway-yocto-meta-openembedded.git"
     [yocto/meta-gardena]="smart-garden-gateway-yocto-meta-gardena.git"
     [yocto/meta-distribution]="smart-garden-gateway-yocto-meta-distribution.git"
@@ -19,16 +20,15 @@ declare -A UPSTREAM_REPOS=(
     [yocto/meta-swupdate]="smart-garden-gateway-yocto-meta-swupdate.git"
     [yocto/openembedded-core]="smart-garden-gateway-yocto-openembedded-core.git"
     [yocto/bitbake]="smart-garden-gateway-yocto-bitbake.git"
-    [yocto/meta-lemonbeat-firmware]="smart-garden-gateway-yocto-meta-lemonbeat-firmware-public.git"
     [yocto/meta-rust]="smart-garden-gateway-yocto-meta-rust.git"
     [.]="smart-garden-gateway-public.git"
     )
 
 scriptdir=$(dirname "$0")
-for repo in "${!UPSTREAM_REPOS[@]}"; do
+for repo in "${!PUBLIC_REPOS[@]}"; do
     echo "$repo"
-    (cd "$scriptdir/$repo" && (git config --get remote.rs.url || git remote add rs "${base}/${UPSTREAM_REPOS[$repo]}"))
+    (cd "$scriptdir/$repo" && (git remote set-url public "${base}/${PUBLIC_REPOS[$repo]}" || remote add public "${base}/${PUBLIC_REPOS[$repo]}"))
 done
 
-git submodule foreach "git push rs \"${release_tag}\""
-git push rs master "${release_tag}"
+git submodule foreach "git push public \"${release_tag}\""
+git push public master "${release_tag}"
